1eec6e19f2e0f28960d9c77b63812a44f3bd7d3c
Fred Danjuma <freddanjuma247@gmail.com>
2025-08-05 09:51:57 -0400

    Implement Book, EBook, PrintBook classes and Library with composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,47 +1,37 @@
-# Base class
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
-# Derived class 1: EBook
 class EBook(Book):
-    def __init__(self, title, author, file_size_kb):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size_kb = file_size_kb
+        self.file_size = file_size  # in KB
 
-    def get_info(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size_kb}KB"
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
-# Derived class 2: PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
-# Composition: Library has a collection of books
 class Library:
     def __init__(self):
-        self.books = []
+        self._books = []
 
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book)
-        else:
-            print("Only Book objects can be added.")
+        self._books.append(book)
 
     def list_books(self):
-        if not self.books:
-            print("The library is empty.")
-        else:
-            for book in self.books:
-                print(book.get_info())
+        for book in self._books:
+            print(book)
